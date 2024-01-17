c66c73dd5c21a9590d864548f6828ab0f34f5ee6
Dilsin Amirtharajah <59810782+Dilsinnam@users.noreply.github.com>
2024-01-16 19:52:56 -0500

    Updated program

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,9 +1,22 @@
-quiz = 0.90
-lab = 0.99
-assignment = 0.96
-exam = 0.89
-examTwo = 0.70
-final = 0.90
-decimal = quiz * lab * assignment * exam * examTwo * final
-finalScore = decimal * 100
-print("The final grade for the class is:", finalScore)
+print(
+    "This is a score calculator for COP3502C. All values have been adjusted as of 1/16/2024 at 7:43 PM EST."
+)
+assignment = float(input("Please enter your assignment grade: "))
+exam = float(input("Please enter your exam grade: "))
+lab = float(input("Please enter your lab grade: "))
+quiz = float(input("Please enter your quiz grade: "))
+project = float(input("Please enter your project grade: "))
+gProject = float(input("Please enter your group project grade: "))
+zyBooks = float(input("Please enter your ZyBooks grade: "))
+lecture = float(input("Please enter lecture grade: "))
+
+totalScore = (
+    assignment * 0.16
+    + exam * 0.30
+    + lab * 0.16
+    + quiz * 0.16
+    + gProject * 0.10
+    + zyBooks * 0.2
+    + lecture * 0.2
+)
+print("Your score for this semester is:", totalScore)
--- readme.md
+++ readme.md
@@ -5,7 +5,14 @@ A very simple grade calculator made with Python, as in intro to COP3502.
 ## Result
 
 ```python
-print("The final grade for the class is:", finalScore)
+assignment = float(input("Please enter your assignment grade: "))
+exam = float(input("Please enter your exam grade: "))
+lab = float(input("Please enter your lab grade: "))
+etc, you get the idea
 
-# The final grade for the class is: 47.9600352
+#Combining the scores
+totalScore = assignment*0.16 + exam*0.30 + lab*0.16 + quiz*0.16 + gProject*0.10 + zyBooks*0.2 + lecture*0.2
+
+#The final score
+print("Your score for this semester is:", totalScore)
 ```
